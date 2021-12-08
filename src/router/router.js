a6219c8f8595f477f298f182081a47fed8f18d00
aquiwer <70368630+aquiwer@users.noreply.github.com>
2021-12-08 13:37:57 +0100

    test

---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import {Registration} from "../components/Registration";
+
+
+export const routes = [
+    {
+        path: '/registration',
+        exact: true,
+        component: Registration,
+        title: "Register",
+        id: 0
+    },
+
+]
+
